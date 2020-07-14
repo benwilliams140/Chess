d12a78e54fda193b9e9c5f0fad8bbe4e2d9205f2
benwilliams140 <ben.williams14@outlook.com>
2020-07-14 19:22:01 -0400

    Fixed a bug with the pawn movement

--- Chess/Pawn.cpp
+++ Chess/Pawn.cpp
@@ -17,14 +17,17 @@ std::vector<sf::Vector2i> Pawn::getPossibleMoves(Array2D<Piece*>& _board)
 
 	if (_board.inBounds(col, row + dir))
 	{
-		if(!_board[col][row + dir]) _moves.push_back(sf::Vector2i(col, row + dir));
-		if (col - 1 >= 0)
-			if (_board[col - 1][row + dir]) _moves.push_back(sf::Vector2i(col - 1, row + dir));
-		if (col + 1 < _board.getCols())
-			if (_board[col + 1][row + dir]) _moves.push_back(sf::Vector2i(col + 1, row + dir));
+		if (!_board[col][row + dir])
+		{
+			_moves.push_back(sf::Vector2i(col, row + dir));
+			if (move == 0 && !_board[col][row + 2 * dir]) _moves.push_back(sf::Vector2i(col, row + 2 * dir));
+		}
+			
+		if (col - 1 >= 0 && _board[col - 1][row + dir] && _board[col - 1][row + dir]->getColour() != getColour())
+			_moves.push_back(sf::Vector2i(col - 1, row + dir));
+		if (col + 1 < _board.getCols() && _board[col + 1][row + dir] && _board[col + 1][row + dir]->getColour() != getColour())
+			_moves.push_back(sf::Vector2i(col + 1, row + dir));
 	}
-
-	if (move == 0 && !_board[col][row + 2 * dir]) _moves.push_back(sf::Vector2i(col, row + 2 * dir));
 
 	return _moves;
 }
