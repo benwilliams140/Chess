e11678a92efad7b3619b5144ac7cdb179e6afbad
benwilliams140 <ben.williams14@outlook.com>
2020-07-14 19:16:09 -0400

    Updated the Rook movement; no longer returns all moves, only moves that are allowed (no need to filter)

--- Chess/Rook.cpp
+++ Chess/Rook.cpp
@@ -15,14 +15,45 @@ std::vector<sf::Vector2i> Rook::getPossibleMoves(Array2D<Piece*>& _board)
 {
 	std::vector<sf::Vector2i> _moves;
 
-	for (int _col = 0; _col < _board.getCols(); ++_col)
-	{
-		if (_col != col) _moves.push_back(sf::Vector2i(_col, row));
-	}
+	int _colBounds[2];
+	int _rowBounds[2];
 
-	for (int _row = 0; _row < _board.getRows(); ++_row)
+	_colBounds[0] = _rowBounds[0] = -1;
+	_colBounds[1] = _rowBounds[1] = _board.getCols();
+
+	for (int _delta = 1; _delta < _board.getCols(); ++_delta)
 	{
-		if (_row != row) _moves.push_back(sf::Vector2i(col, _row));
+		for (int _multiple = -1; _multiple <= 1; _multiple += 2)
+		{
+			int _index = _multiple < 0 ? 0 : 1;
+			int _nullBounds = _multiple < 0 ? -1 : _board.getCols();
+
+			if (_board.inBounds(col + _delta * _multiple, row))
+			{
+				if (_board[col + _delta * _multiple][row] && _colBounds[_index] == _nullBounds)
+				{
+					_colBounds[_index] = col + _delta * _multiple;
+					if (_board[col + _delta * _multiple][row]->getColour() == getColour())
+						_colBounds[_index] -= _multiple;
+				}
+
+				if ((_multiple < 0 && col + _delta * _multiple >= _colBounds[_index]) || (_multiple > 0 && col + _delta * _multiple <= _colBounds[_index]))
+					_moves.push_back(sf::Vector2i(col + _delta * _multiple, row));
+			}
+
+			if (_board.inBounds(col, row + _delta * _multiple))
+			{
+				if (_board[col][row + _delta * _multiple] && _rowBounds[_index] == _nullBounds)
+				{
+					_rowBounds[_index] = row + _delta * _multiple;
+					if (_board[col][row + _delta * _multiple]->getColour() == getColour())
+						_rowBounds[_index] -= _multiple;
+				}
+
+				if ((_multiple < 0 && row + _delta * _multiple >= _rowBounds[_index]) || (_multiple > 0 && row + _delta * _multiple <= _rowBounds[_index]))
+					_moves.push_back(sf::Vector2i(col, row + _delta * _multiple));
+			}
+		}
 	}
 
 	return _moves;
